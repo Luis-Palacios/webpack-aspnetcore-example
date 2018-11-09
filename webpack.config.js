a972c815bf1221e55ee8abb611e1096e1fb159d5
Luis Palacios <lrpalaciosdev@gmail.com>
2018-11-09 12:05:43 -0600

    more cleanup

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './wwwroot/app/home.js',
-    output: {
-        filename: 'homebundle.js',
-        path: path.resolve(__dirname, 'wwwroot', '.temp'),
-    }
-}
