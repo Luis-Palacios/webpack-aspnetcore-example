7bfbe4a230c834109f57db1f6d060aa7e68e7e5c
Luis Palacios <lrpalaciosdev@gmail.com>
2019-07-19 23:46:56 -0600

    add basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'wwwroot', 'app', 'home.js'),
+  output: {
+    filename: 'homebundle.js',
+    path: path.resolve(__dirname, 'wwwroot', '.temp')
+  }
+};
